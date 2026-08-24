c6a86fbeca6124d8e279cabf2015ae819a2b15b0
seonj9190 <seonj9190@gmail.com>
2026-08-24 23:03:38 +0900

    Updates

---
+++ assets/performances-BZqLEkov.js
@@ -0,0 +1,1 @@
+var e=[{id:1,title:`서귀포 챔버 오케스트라 창단연주회`,date:`2026-11-03`,time:`18:00`,venue:`서귀포예술의전당 대극장`,program:`드보르자크 현악 세레나데 외`,description:`가을을 주제로 한 현악 레퍼토리로 구성한 정기연주회입니다.`,poster:`https://picsum.photos/seed/sgp-perf-01/600/800`}];export{e as t};
